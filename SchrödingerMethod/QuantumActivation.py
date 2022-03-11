c749aa9f5eccc8f22040680f6686fc274078bfac
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-03-11 19:17:11 +0900

    update

---
+++ SchrödingerMethod/QuantumActivation.py
@@ -0,0 +1,6 @@
+# ---------------------------------
+# SchrodingerNet: Quantized Neural Network
+# Quantum Activation
+# Copyright and Written By Suhyung Choi
+# Licensed under the MIT License
+# ---------------------------------
--- SchrödingerMethod/QuantumNormalization.py
+++ SchrödingerMethod/QuantumNormalization.py
@@ -1,1 +1,8 @@
+# ---------------------------------
+# SchrodingerNet: Quantized Neural Network
+# Quantum Activation
+# Copyright and Written By Suhyung Choi
+# Licensed under the MIT License
+# ---------------------------------
+
 # Unify the normalization and activation
